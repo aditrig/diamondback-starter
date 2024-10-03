1f5e273fad9f4a325e96a0c0e1f28f9618079190
Aditri Gupta <aditri@aditris-mbp-2.wifi.local.cmu.edu>
2024-10-03 10:32:09 -0400

    inital commit- added files

--- runtime/start.rs
+++ runtime/start.rs
@@ -6,25 +6,42 @@ extern "C" {
     // it does not add an underscore in front of the name.
     // Courtesy of Max New (https://maxsnew.com/teaching/eecs-483-fa22/hw_adder_assignment.html)
     #[link_name = "\x01our_code_starts_here"]
-    fn our_code_starts_here(input: u64) -> u64;
+    fn our_code_starts_here(input: i64) -> i64;
 }
 
 #[export_name = "\x01snek_error"]
 pub extern "C" fn snek_error(errcode: i64) {
     // TODO: print error message according to writeup
-    eprintln!("an error ocurred {errcode}");
+    match errcode {
+        0 => eprintln!("an error ocurred: numeric operation resulted in overflow"),
+        _ => ()
+    }
     std::process::exit(1);
 }
 
 #[export_name = "\x01snek_print"]
-pub extern "C" fn snek_print(value: i64, type_flag: u64) {
-    // TODO: print the value according to the writeup
+pub extern "C" fn snek_print(value: i64, type_flag: i64) {
+    fn inner_print(value: i64, type_kind: i64) -> i64{
+        if type_kind == 0 {
+            println!("{}", value);
+        } else {
+            if value == 0 {
+                println!("false")
+            } else {
+                println!("true")
+            }
+        }
+            return value
+    }
+    inner_print(value,type_flag);
 }
 
 
-fn parse_input(input: &str) -> u64 {
-    // TODO: parse the input string into internal value representation
-    0
+fn parse_input(input: &str) -> i64 {
+    // ADDED parse input number
+    // CHANGE: printing the input was causing error so removed it
+    let input_val_int: i64 = input.parse().expect("Input is not a valid i64");
+    input_val_int
 }
 
 fn main() {
@@ -32,6 +49,7 @@ fn main() {
     let input = if args.len() == 2 { &args[1] } else { "0" };
     let input = parse_input(&input);
 
-    let i: u64 = unsafe { our_code_starts_here(input) };
-    println!("{i}");
+    let _i: i64 = unsafe { our_code_starts_here(input) };
+    // println!("{i}");
 }
+
